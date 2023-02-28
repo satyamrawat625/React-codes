6e9cd5636b84c0d17760250675b91a95ed6afbb8
Satyam Rawat <78161825+satyamrawat625@users.noreply.github.com>
2023-02-28 19:24:49 +0530

    Add files via upload

---
+++ l37 changing complex state,chng form with input val/src/components/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function App() {
+  const [fullName, setFullname] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+
+    setFullname((prevValue) => {
+      if (inputName === "fName") {
+        return {
+          fName: newValue,
+          lName: prevValue.lName
+        };
+      } else if (inputName === "lName") {
+        return {
+          fName: prevValue.fName,
+          lname: newValue
+        };
+      }
+    });
+  }
+  return (
+    <div className="container">
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+      <form>
+        <input
+          name="fName"
+          placeholder="First Name"
+          onChange={handleChange}
+          value={fullName.fName}
+        />
+        <input
+          name="lName"
+          placeholder="Last Name"
+          onChange={handleChange}
+          value={fullName.lName}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
