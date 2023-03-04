de0fcabba712aa84599a982fbe3ffb1aeb8148c9
Satyam Rawat <78161825+satyamrawat625@users.noreply.github.com>
2023-03-05 00:18:43 +0530

    Add files via upload

---
+++ l43 keeper app part3/src/components/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    // console.log("delete trigged");
+    setNotes((prevNotes) => {
+      //dlt ind whose id matches
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id; //return ind whose id not matches
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
